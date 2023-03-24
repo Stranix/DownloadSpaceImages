620bded57264ca2a662f749977c0801144341b60
Stranix <Phantom2525@gmail.com>
2023-03-24 16:03:32 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,10 +2,13 @@ import os
 
 from telegram import Bot
 from dotenv import load_dotenv
+from pathlib import Path
 
 if __name__ == '__main__':
     load_dotenv()
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
     bot = Bot(token=BOT_TOKEN)
+    chat_id = -1001902840562
 
-    bot.send_message(chat_id=-1001902840562, text='Отправка сообщения в канал')
+    path_to_image = Path('./images/nasa_apod_4.jpg')
+    bot.send_photo(chat_id, open(path_to_image, 'rb'))
