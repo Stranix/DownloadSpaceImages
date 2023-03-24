95484e78748417574aa397f1380a258c799af97b
Stranix <Phantom2525@gmail.com>
2023-03-24 15:52:11 +0300

    publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,3 +7,5 @@ if __name__ == '__main__':
     load_dotenv()
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
     bot = Bot(token=BOT_TOKEN)
+
+    bot.send_message(chat_id=-1001902840562, text='Отправка сообщения в канал')
