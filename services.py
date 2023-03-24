3ec905e7c3a88f0f52acd306ede19c7f4e5a2681
Stranix <Phantom2525@gmail.com>
2023-03-24 15:17:19 +0300

    services: Delete debug message

--- services.py
+++ services.py
@@ -4,15 +4,10 @@ from pathlib import Path
 
 
 def download_image(image_url: str, path_to_save: str, image_name: str, params=None):
-    print('Загрузка картинке по ссылке:', image_url)
+
     response = requests.get(image_url, params=params)
     response.raise_for_status()
-
-    print('DEBUG response_status_code:', response.status_code)
 
     Path(path_to_save).mkdir(exist_ok=True)
     with open(f'{path_to_save}/{image_name}', 'wb') as image_file:
         image_file.write(response.content)
-
-    print('Загрузка завершена')
-    print('-' * 25)
