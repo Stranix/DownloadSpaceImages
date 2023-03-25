ad1cc87b847966bf9202fe3f59c7c7fb93c259f1
Stranix <Phantom2525@gmail.com>
2023-03-25 12:59:11 +0300

    download_image: create folder with subfolder

--- services.py
+++ services.py
@@ -8,6 +8,6 @@ def download_image(image_url: str, path_to_save: str, image_name: str, params=No
     response = requests.get(image_url, params=params)
     response.raise_for_status()
 
-    Path(path_to_save).mkdir(exist_ok=True)
+    Path(path_to_save).mkdir(exist_ok=True, parents=True)
     with open(f'{path_to_save}/{image_name}', 'wb') as image_file:
         image_file.write(response.content)
